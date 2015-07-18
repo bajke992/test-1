702476b46ef543a4a4c8b4fd03902e3ef435c7a2
Aleksandar Bajkin <salebdj@gmail.com>
2015-07-18 17:39:47 +0200

    removing autopublish and adding publish and subscribe actions

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,18 +1,5 @@
-var postsData = [
-	{
-		title: 'Introduction',
-		url: 'http://test.com/intro/',
-	},
-	{
-		title: 'Test 1',
-		url: 'http://url.com/test-1/',
-	},
-	{
-		title: 'Test 2',
-		url: 'http://url.com/test-2/',
-	},
-];
-
 Template.postsList.helpers({
-	posts: postsData
+	posts: function(){
+		return Posts.find();
+	}
 });
