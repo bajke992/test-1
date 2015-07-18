4da0a50baf9df49daf603af1bfd5bd5c75a4e8d7
Aleksandar Bajkin <salebdj@gmail.com>
2015-07-19 00:34:46 +0200

    Facebook login and other stuffs

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,14 +1,14 @@
 if(Posts.find().count() === 0){
 	Posts.insert({
 		title: 'Post 1',
-		url: '/post-1/'
+		url: 'post-1'
 	});
 	Posts.insert({
 		title: 'Post 2',
-		url: '/post-2/'
+		url: 'post-2'
 	});
 	Posts.insert({
 		title: 'Post 3',
-		url: '/post-3/'
+		url: 'post-3'
 	});
 }
---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+	service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+	service: 'facebook',
+	appId: '1613189092292217',
+	secret: 'bd47ebf45cff4c60ed81d1d1f47fe61b'
+});
